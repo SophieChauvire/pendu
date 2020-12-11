75b40c941b946592b6815e95acea41daae5c824a
SophieChauvire <sophie.chauvire@cpe.fr>
2020-12-11 07:47:25 +0100

    maj des fichiers

--- fonctions.py
+++ fonctions.py
@@ -1,31 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Ce programme contient les focntions nécessaires pour jouer au jeu du pendu
+@author: sophie chauviré
 Created on Fri Nov 27 10:55:22 2020
-
-@author: sophie
 """
 
-import random as rd
+
+"""ce programme s'appuie sur les bibliothèqes random et string
+ainsi que sur le fichier mot.py
+"""
+from random import randint
 import string
-from mots import*
-from jeu_pendu import*
+from paramètres_pendu import *
+
 
 def choix_mot():
-    m=rd.randint(0,len(lines))
+    """
+    cette fonction choisit un mot parmis la liste lines définie dans le fichier paramètres_pendu.py
+    entré : aucune
+    sortie : renvoie un élement de type str de la liste lines
+    """
+    m=randint(0,len(lines))
     return lines[m]
  
 
 def entrer_lettre():
+    """
+    cette fonction renvoie une lettre entrée par l'utilisateur, si ce qu'entre l'utilisateur n'est pas 
+    une lettre la fonction est rappelée jusqu'à avoir une lettre
+    entrée : aucune
+    sortie : renvoie une lettre majusucule
+    """
     lettre = input("Entrez une lettre : ")
     if lettre not in string.ascii_letters or len(lettre)>1:
-        print("Merci d'entrer une lettre")
+        print("Merci d'entrer une lettre valide")
         return entrer_lettre()
     else:
         lettre = lettre.upper()
     return lettre
     
+
 def renvoyer_mot(mot_choisi,liste_lettres):
+    """
+    cette fonction renvoit un mot partiellement construit en fonction des lettres trouvées par l'utilisateur et du mot à trouver
+    entrées : le mot à trouver qui a été choisi par la machine
+            la liste des lettres contenues dans le mot, trouvées par l'utilisateur
+    sortie : renvoie un mot partiellement contruit, affichant les lettres trouvées par l'utilisateur et des '_' pour les lettres qui restent à trouver
+    """
     mot_partiel=''
     for i in mot_choisi:
         if i in liste_lettres :
@@ -34,8 +56,3 @@ def renvoyer_mot(mot_choisi,liste_lettres):
             mot_partiel+=' _'
     return mot_partiel
 
-def test_lettre(lettre,mot_choisi):
-    if lettre in mot_choisi:
-        liste_lettres.append(lettre)
-    else:
-        liste_lettres_fausses.append(lettre)
